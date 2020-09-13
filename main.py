92bfdbc1b0cbb81a7657e8e94eb93c580caf6812
storrellas <storrellas@gmail.com>
2020-09-13 23:22:39 +0000

    ENH: Added first calls to API

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello world")
+import requests
+import json
 
 
 # Import localsettings if any
@@ -7,4 +8,38 @@ try:
 except ImportError:
     pass
 
-print("API_TOKEN ", API_TOKEN)
+# Fetch Inbox 
+######################
+# https://manybrain.github.io/m8rdocs/#fetch-inbox-aka-fetch-message-summaries
+
+#test1@storrellasteam.m8r.co
+INBOX = 'test1'
+DOMAIN = 'storrellasteam.m8r.co'
+
+headers = {'Authorization': API_TOKEN}
+response = requests.get(f'https://mailinator.com/api/v2/domains/{DOMAIN}/inboxes/{INBOX}?limit=2&sort=descending', headers=headers)
+json_data = json.loads(response.text)
+
+# JSON Data
+print(json.dumps(json_data, indent=4))
+
+# Fetch Message
+######################
+# https://manybrain.github.io/m8rdocs/#fetch-message
+
+message = json_data['msgs'][0]
+message_id = message['id']
+
+response = requests.get(f'https://mailinator.com/api/v2/domains/{DOMAIN}/inboxes/{INBOX}/messages/{message_id}', headers=headers)
+print("fetch_message ", response.text, message_id)
+json_data = json.loads(response.text)
+
+# JSON Data
+print(json.dumps(json_data, indent=4))
+
+
+# Fetch SMS Message
+######################
+# https://manybrain.github.io/m8rdocs/#fetch-an-sms-messages
+
+
